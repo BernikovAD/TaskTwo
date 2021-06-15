eb5dc97914816a1fbc4382fbda05e83c153f7615
Aleksandr Bernikov <bernikov_1993@mail.ru>
2021-06-15 23:35:26 +0500

    TaskThree

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.test;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.annotation.IntRange;
 import androidx.annotation.NonNull;
@@ -13,12 +16,29 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String APP_PREFERENCES = "mysettings";
+    public static final String APP_PREFERENCES_COUNT = "count";
+    SharedPreferences mSettings;
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
+
+        int count=0;
+        count = mSettings.getInt(APP_PREFERENCES_COUNT,0);
+        if (savedInstanceState == null) {
+            count++;
+            SharedPreferences.Editor editor = mSettings.edit();
+            editor.putInt(APP_PREFERENCES_COUNT, count);
+            editor.apply();
+        }
+        if (count % 3 == 0) {
+            Toast.makeText(this,
+                    "Это третий холодный запуск приложения!", Toast.LENGTH_LONG).show();
+
+        }
         int[] price = new int[]{5, 100, 20, 66, 16};
         decryptData(price, 50, 1, 3);
         decryptData(price, 33, 0, 5);
